69edf4941dfa4fbecf7f04e186a636af4a7c5438
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-06-18 08:40:19 +0530

    Add files via upload

---
+++ jasonforeach.js
@@ -0,0 +1,10 @@
+var obj = [
+          { person: "Name 1", age: "2", company: "GUVI" },
+          { person: "Name 2", age: "5", company: "GUVI geek" },
+          { person: "Name 3", age: "8", company: "GUVI geek network" },
+        ]
+        obj.forEach(obj => {
+        Object.entries(obj).forEach(([key, value]) => {
+            console.log(`${key} : ${value}`);
+        });
+    });
