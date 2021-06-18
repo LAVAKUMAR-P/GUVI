2228d78b1940098d0c80446008ad0eb0000e0c1c
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-06-18 08:11:32 +0530

    Create jasonforiloop.js

---
+++ jasonforiloop.js
@@ -0,0 +1,16 @@
+var obj = [
+          { person: "Name 1", age: "2", company: "GUVI" },
+          { person: "Name 2", age: "5", company: "GUVI geek" },
+          { person: "Name 3", age: "8", company: "GUVI geek network" },
+        ]
+        let keys=[]
+        
+        for(let i=0;i<obj.length;i++)
+        {
+            keys=Object.keys(obj[i]);
+            for(let j=0;j<keys.length;j++)
+            {
+                let key=keys[j];
+                console.log(key+":"+obj[i][key]);
+            }
+        }
