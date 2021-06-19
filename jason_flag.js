ae33d7c64488d336fc50a64a55ad99afd3e6e5b5
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-06-19 14:35:03 +0530

    Add files via upload

---
+++ jason_flag.js
@@ -0,0 +1,19 @@
+var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://restcountries.eu/rest/v2/all");
+        xhr.onload = function () {
+          var data = JSON.parse(this.response);
+          console.log(data);
+          for(let i in data)
+          {
+          for(let j in data[i])
+          {
+           
+          if(j==="flag")
+          {
+          
+          console.log(j+":"+data[i][j]);
+          }
+          }
+          }
+        };
+        xhr.send();
