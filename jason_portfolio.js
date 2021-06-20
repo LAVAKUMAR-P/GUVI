a8a91d25749975bb6db14889eceef840914a109c
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-06-20 09:27:24 +0530

    Add files via upload

---
+++ jason_portfolio.js
@@ -0,0 +1,26 @@
+let obj={
+name:"LAVAKUMAR.P",
+"ROLL NO": "17MEO52",
+ DEGREE : 'BACHELOR OF ENGINEERING (B.E) ',
+ BRANCH :'MECHANICAL ENGINEERING',
+ COLLEGE:'K.S.R COLLEGE OF ENGINEERING',
+'Father’s name':'Pazhaniappan S',
+Gender:'Male',
+'Date of Birth ':"16th Jan 2000",
+"Languages known":"Tamil, English",
+Email:"lavakumar16000@gmail.com",
+Mobile:"+91-7339238342",
+"Permanent Address":"8/144, Kanakkuppillai Kadu, Vananasi (P.O), Salem-636457 Tamil Nadu.",
+"CAREER OBJECTIVE":"I seek to work in a competitive field at a recognized organization by using my skills and abilities to achieve my vision with perseverance and determination.",
+"ACADEMIC RECORD":"Course:B.E, Institution: K.S.R College of engineering Tiruchencgode. Board/ University: Anna University, Completion By : 2021*,Marks(%/CGPA): 9.15*",
+"FIELD OF INTEREST":" Programming ",
+"WORKSHOP ATTENDED":" Two day international workshop on IC engines(PISTON-2018) at ICSA SEMINAR HALL & IIT Madras Research park The objective of this workshop is to learn about various types of engine parts & hands on training to assemble & dismantle the four stroke IC engine ",
+"ACHIEVEMENTS AND EXTRA-CURRICULAR ACTIVITIES":"Responsibilities: Served as Class Representative from 1st to 4th year ,Co-curricular activities & achievements:Learning Catia V5 R20 (surface design),Secured first place in the I to VII semester examinations,Active member in ISHRAE KSRCE students’ chapter ,Extra-curricular activities:Active member at JRC in school ",
+"ACADEMIC PROJECTS":" To Compare the of strength and stiffness of a banyan tree fiber with synthetic fiber (under the guidance of prof. Dr. M. Prabu) The objective of this project is to compare the mechanical properties of Natural (banyan tree) and synthetic fiber for the application of automotive industries. The following tests are performed to compare the mechanical properties of natural and synthetic fiber tensile test, Impact test & compression test. ",
+HOBBIES:"Reading about new technology ",
+DECLARATION :" I, Lavakumar P, do hereby confirm that the information given above is true to the best of my knowledge.",
+};
+for(let k in obj)
+{
+console.log(k+":"+"  "+obj[k]);
+}
