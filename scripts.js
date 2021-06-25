d723303ac590507a567607ff1f45b0927c47a430
LAVAKUMAR-P <84187714+LAVAKUMAR-P@users.noreply.github.com>
2021-06-25 21:07:27 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,160 @@
+// this line is not having semicolon
+console.log("1) this line is not having semicolon")
+
+/*alert("Im invoked!");
+alert("I'm JavaScript!");
+alert('Hello') // this line is not having semicolon
+alert(`1)Wor
+ ld`)
+alert(3 +
+1
++ 2); // this is multiple line code and its working*/
+console.log("----------------------------------------------------------------------------");
+//2)Fix the below to alert hello Guvi geek
+console.log("2)Fix the below to alert hello Guvi geek");
+/*
+fname = "Guvi";
+lname = "geek"
+admin = fname+lname;
+//alert( admin ); // "Guvi geek"
+alert( 'hello'+" "+admin);*/
+console.log("----------------------------------------------------------------------------");
+//3)alert sum of two numbers
+console.log("3)alert sum of two numbers");
+/*let a = prompt("First number?");
+let b = prompt("Second number?");
+alert(+a + +b);*/
+console.log("----------------------------------------------------------------------------");
+console.log("4)Code is blasted and how to diffuse it and get Diffused");
+//Code is blasted and how to diffuse it and get “Diffused”.
+/*var a = 2 >12;
+console.log(a);
+//Don't touch below this
+if (a) {
+  console.log("Code is Blasted")
+}
+else
+{
+  console.log("Diffused") 
+}*/
+console.log("----------------------------------------------------------------------------");
+console.log("4)How to get the success in console.");
+//How to get the success in console.
+/*let a = prompt("Enter a number?");
+//Don't modify any code below this
+if (a) {
+ console.log( 'OMG it works for any number inc 0' );
+}
+else
+{
+ console.log( "Success" );
+}*/
+//if we didnot give any input in prompt it will display success
+console.log("----------------------------------------------------------------------------");
+console.log("5)How to get the correct score in console.");
+//How to get the correct score in console.
+/*let value = prompt('How many runs you scored in this ball');
+if (+value === 4) {
+      console.log("You hit a Four");
+} else if (+value === 6) {
+      console.log("You hit a Six");
+} else {
+      console.log("I couldn't figure out");
+}*/
+console.log("----------------------------------------------------------------------------");
+console.log("6)Fix the code to welcome the Employee");
+//Fix the code to welcome the Employee
+/*let login = 'Employee';
+let message = (login == 'Employee');
+console.log('welcome');*/
+console.log("----------------------------------------------------------------------------");
+console.log("7)Fix the code to welcome the boss");
+//Fix the code to welcome the boss
+/*let a1=prompt("Are you boss put 1 if u r boss else put 0");
+if (+a1==1)
+ message = "welcome boss";
+else
+ message = "Go away";
+  console.log(message);*/
+console.log("----------------------------------------------------------------------------");
+console.log("7)Fix the code to welcome the boss");
+//Fix the code to welcome the boss
+//let message;
+//let lock =false;
+//Dont change any code below this 
+/*if (null || lock || undefined )
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+  console.log(message);*/
+console.log("----------------------------------------------------------------------------");
+console.log("8)Fix the code to welcome the boss");
+//Fix the code to welcome the boss
+/*let message;
+let lock =false;
+//Dont change any code below this
+if (lock && " " || undefined )
+{
+  message = "Go away";
+}
+else
+{
+ message = "welcome";
+}
+console.log(message);*/
+console.log("----------------------------------------------------------------------------");
+console.log("9)Change the code to print 3 2 1");
+//Change the code to print 3 2 1
+//You can change only 2 characters
+/*let i = 3;
+while (i>=1) {
+  console.log( i-- );
+}*/
+console.log("----------------------------------------------------------------------------");
+console.log("10)Change the code to print 1 to 10 in 4 lines");
+//Change the code to print 1 to 10 in 4 lines
+/*let num=1
+while(num<=10)
+{
+console.log(num++);}
+//Change the code to print even numbers
+//You are allowed to modify only one character 
+for (let num = 2; num <= 20; num += 2) {
+    console.log(num)
+  }*/
+console.log("----------------------------------------------------------------------------");
+console.log("11)Change the code to print all the gifts");
+//Change the code to print all the gifts
+/*let gifts = ["teddy bear", "drone", "doll"];
+for (let i = 0; i < 3; i++) {
+  console.log('Wrapped'+' '+gifts[i]+" "+"and added a bow!");
+}*/
+console.log("----------------------------------------------------------------------------");
+console.log("12)Fix the code to disarm the bomb");
+//Fix the code to disarm the bomb.
+/*let countdown = 100;
+while (countdown <0) {
+  countdown--;
+  if(countdown == 0)
+  {
+   console.log("bomb triggered");
+  }
+}*/
+console.log("----------------------------------------------------------------------------");
+console.log("13)Whats the msg printed and why?");
+//Whats the msg printed and why?
+var a ='1';
+console.log(typeof(a));
+var b= 0;
+var msg ='';
+if (a) {
+ msg += 'hi';
+ }
+if (b) {
+ msg += 'Hello';
+}
+console.log(msg);
